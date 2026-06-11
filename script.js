3019c65efa879c4bdf1b7f1c49ab56a3de754b4c
hitesh <bhatnagerhitesh@gmail.com>
2026-06-11 11:16:19 +0530

    2nd push

--- script.js
+++ script.js
@@ -2,28 +2,32 @@ let input = document.querySelector(".guess-input")
 let feedback = document.querySelector(".feedback-text")
 let checkButton = document.querySelector(".btn-primary")
 let restart = document.querySelector(".btn-secondary")
+let prevGuess = document.querySelector(".prev-guess span")
 let attemptsText = document.querySelector(".attempts-text span")
+let form = document.querySelector(".game-form")
 
 let userArray = []
 let attempts = 5
 let randomNumber = Math.floor(Math.random() * 100) + 1
-restart.disabled  = true
+restart.disabled = true
+restart.classList.add('hidden')
 
 attemptsText.innerText = attempts
 
 function gameFunction() {
-    let userInput = Number(input.value)
-    if (attempts > 0) {
+    let userInput = Math.abs(Number(input.value))
+    let close = randomNumber - userInput
+    if (attempts !== 0) {
         if (userInput != randomNumber) {
-            if (userInput > randomNumber) {
+            if (close <= 5) {
+                feedback.innerText = "very close"
+            } else if (userInput > randomNumber) {
                 feedback.innerText = "too high"
-                input.value = ""
-                attempts = attempts - 1
             } else if (userInput < randomNumber) {
                 feedback.innerText = "too low"
-                input.value = ""
-                attempts--
             }
+            input.value = ""
+            attempts--
             attemptsText.innerText = `${attempts}`
             userArray.push(userInput)
         } else if (userInput === randomNumber) {
@@ -33,18 +37,24 @@ function gameFunction() {
             attemptsText.innerText = `${attempts}`
             checkButton.disabled = true
             restart.disabled = false
+            userArray.push(userInput)
+            restart.classList.remove('hidden')
         }
-    } else {
+    } else if (attempts === 0) {
         feedback.innerText = `game over number is ${randomNumber}`
         input.readOnly = true
         input.value = ""
         attemptsText.innerText = `${attempts}`
         checkButton.disabled = true
         restart.disabled = false
+        userArray = []
+        restart.classList.remove('hidden')
     }
+    prevGuess.innerText = userArray
 }
 
-checkButton.addEventListener('click', () => {
+form.addEventListener('submit', (e) => {
+    e.preventDefault()
     gameFunction()
 })
 
@@ -57,4 +67,9 @@ restart.addEventListener('click', () => {
     input.value = ""
     feedback.innerText = `guess the new number`
     attemptsText.innerText = `${attempts}`
+    prevGuess.innerText = ""
+    userArray = []
+    restart.classList.add("hidden")
 })
+
+console.log(randomNumber)
